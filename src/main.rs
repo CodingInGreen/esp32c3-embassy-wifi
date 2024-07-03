df30c042b69930e66d2fa685fdc0d5f39e7db60c
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-07-03 15:27:54 +0100

    Update error.txt

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ use esp_wifi::{
 };
 
 macro_rules! mk_static {
-    ($t:ty,$val:expr) => {{
+    ($t:path,$val:expr) => {{
         static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
         #[deny(unused_attributes)]
         let x = STATIC_CELL.uninit().write(($val));
